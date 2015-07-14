07e8a354c6d299353e0cbcf3e45a0438517ef927
Jouke van der Maas <j.maas@niposoftware.com>
2015-07-14 21:54:03 +0200

    Create setup_vim.sh

---
+++ setup_vim.sh
@@ -0,0 +1,16 @@
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle
+
+git clone https://github.com/ntpeters/vim-better-whitespace
+git clone https://github.com/mustache/vim-mustache-handlebars
+git clone https://github.com/tpope/vim-sensible
+git clone https://github.com/sjl/badwolf
+git clone https://github.com/scrooloose/nerdtree
+git clone https://github.com/kien/ctrlp.vim
+git clone https://github.com/scrooloose/syntastic
+git clone https://github.com/Raimondi/delimitMate
+git clone https://github.com/nathanaelkane/vim-indent-guides
+git clone https://github.com/pangloss/vim-javascript
+git clone https://github.com/jelera/vim-javascript-syntax
